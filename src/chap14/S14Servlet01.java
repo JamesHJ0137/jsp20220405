4ec65d5bb9a34d8d679a47abe5cfcf3ec760f169
JamesHJ0137 <99157645+JamesHJ0137@users.noreply.github.com>
2022-04-21 16:49:40 +0900

    extra

--- src/chap14/S14Servlet01.java
+++ src/chap14/S14Servlet01.java
@@ -1,13 +1,13 @@
 package chap14;
 
-import java.io.IOException;
+import java.io.*;
+import java.sql.*;
 import java.util.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import javax.sql.*;
 
 /**
  * Servlet implementation class S14Servlet01
@@ -15,45 +15,71 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/S14Servlet01")
 public class S14Servlet01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public S14Servlet01() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public S14Servlet01() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ServletContext application = getServletContext();
+		List<String> cities = new ArrayList<>();
 		
 		// database에서 records 가져오기
 		// 1. 연결 설정
-		// 2. statement 객체 생성
-		// 3. 쿼리 생성
-		// 4. 실행결과 정제
-		// 5. 자원 닫기
-		
+		DataSource ds = (DataSource) application.getAttribute("dbpool");
+
+		String sql = "SELECT city FROM Customers";
+		try (
+				Connection con = ds.getConnection();
+				// 2. statement 객체 생성
+				Statement stmt = con.createStatement();
+				// 3. 쿼리 생성
+				ResultSet rs = stmt.executeQuery(sql);) {
+
+			// 4. 실행결과 정제
+			while (rs.next()) {
+				String city = rs.getString(1);
+				cities.add(city);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+	// 5. 자원 닫기
+//		rs.close();
+//		stmt.close();
+//		con.close();
+
 //		List<String> cities = new ArrayList<>();
 //		cities.add("Berlin");
 //		cities.add("London");
 //		cities.add("Madrid");
-		
-		// request에 records 넣기
+
+	// request에 records 넣기
 		request.setAttribute("cities", cities);
-		
-		// jsp로 forward
-		String location = "/WEB-INF/view/chap14/ex01.jsp";
-		request.getRequestDispatcher(location).forward(request, response);
-		
+
+	// jsp로 forward
+	String location = "/WEB-INF/view/chap14/ex01.jsp";
+	request.getRequestDispatcher(location).forward(request,response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
