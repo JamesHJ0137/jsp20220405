b96be965cd2f3539c6af07d9e58684bb433e71d6
JamesHJ0137 <99157645+JamesHJ0137@users.noreply.github.com>
2022-05-03 03:43:44 +0900

    reply servlet

--- WebContent/WEB-INF/sql/31BOARD_APP.sql
+++ WebContent/WEB-INF/sql/31BOARD_APP.sql
@@ -8,11 +8,33 @@ CREATE TABLE Board (
     inserted DATETIME NOT NULL DEFAULT NOW()
 );
 
+DROP TABLE reply;
+
+CREATE TABLE Reply (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	board_id INT, -- foreign key (참조키, 외래키)
+	content VARCHAR(255) NOT NULL,
+    inserted DATETIME DEFAULT NOW(),
+    FOREIGN KEY (board_id) REFERENCES Board(id) -- FOREIGN KEY 제약사항 추가
+);
+
+-- 테이블 관계
+-- 1:1
+-- 학생 (이름, 생년월일...), 학력(출신학교, 졸업년도)
+
+-- 1:n
+-- 게시물(게시물번호, 내용), 댓글 (댓글 내용)
+
+-- n:n
+-- 학생 (이름, 생년월일...), 회사 (회사명, ...)
+
 SELECT * FROM Board;
 
 SET SQL_SAFE_UPDATES = 0;
-
 UPDATE Board 
 SET inserted = date_sub(inserted, INTERVAL 1 DAY);
 
 DELETE FROM Board WHERE id IN (2, 4);
+
+SELECT * FROM Reply;
+
